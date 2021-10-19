97a7626ab60167240d84f4b5221f1c196a4227fe
Kate <nikaterina@gmail.com>
2021-10-19 21:48:38 +0300

    dropdown first logic added

--- src/mixins/dropdown/dropdown.js
+++ src/mixins/dropdown/dropdown.js
@@ -1,1 +1,35 @@
 import "./dropdown.scss";
+import { dropdownMenu } from "./dropdownMenu/dropdownMenu.js";
+
+function dropdown(uniqueId, id) {
+  let amount = 0;
+  const menu = document.querySelector(`#dropdown-menu${id}`);
+
+
+  dropdownMenu(menu, amount);
+  console.log("amounttttt", amount);
+  intoInput(amount);
+}
+
+dropdown('123', '21');
+
+function translate(x){
+   if ((x-1)% 10 === 0 && x !==11){
+     return `${x} Гость`;
+   }
+  if (x <=4){
+    return `${x} Гостя`
+  }
+   if (x === 11 || (x > 4 && x <= 20)) {
+     return `${x} Гостей`;
+   }
+}
+function intoInput(str){
+  console.log("string", str);
+  let string = ''
+if (str !== 0) {
+  string = translate(str);
+  console.log("string", string);
+  return string
+}
+}
--- src/mixins/dropdown/dropdownMenu/dropdownMenu.js
+++ src/mixins/dropdown/dropdownMenu/dropdownMenu.js
@@ -1,1 +1,37 @@
+
 import "./dropdownMenu.scss";
+
+const id = '222'
+const menu = document.querySelector(`#dropdown-menu${id}`)
+let amount = 0
+
+export function dropdownMenu(menu, amount){
+menu.addEventListener("click", function (event) {
+  handleClick(event);
+});
+
+function handleClick(event) {
+  if (event.target.classList.contains("btn_round-minus")) {
+    let data = event.target.nextElementSibling;
+    data.innerHTML = +event.target.nextElementSibling.innerHTML - 1;
+    amount = amount - 1;
+    if (data.innerHTML === "0") {
+      event.target.disabled = true;
+    }
+  }
+
+  if (event.target.classList.contains("btn_round-add")) {
+    let data = event.target.previousElementSibling;
+    data.innerHTML = +event.target.previousElementSibling.innerHTML + 1;
+    amount = amount + 1;
+console.log("amount", amount);
+    if (data.innerHTML === "1") {
+      let buttonMinus = data.previousElementSibling;
+      buttonMinus.disabled = false;
+    }
+  }
+}
+return amount
+}
+dropdownMenu(menu,0);
+
